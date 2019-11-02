c9a2fbe254017cbef5b2c144adc7a8b7c6b62ae4
z28bl <z28blackpearl@gmail.com>
2019-11-02 15:04:02 -0400

    Adding basic intro

---
+++ PyInstaller with Qt5/BasicGui.py
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------------------------------
+# Program: Basic program that will be compiled into an .exe with Py Installer
+# Created by: Otto-G
+# Created on: 2019-11-02
+# License: Unlicense
+# ---------------------------------------------------------------------------------
